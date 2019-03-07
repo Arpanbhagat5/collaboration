ac6c6b73b02241f4972e0a73c9b59986ec3508e4
Ayushi Gupta <39458502+helixRedshift@users.noreply.github.com>
2019-03-07 21:45:18 +0900

    Revert "Shell scripts"

--- rename-files-in-dir.sh
+++ rename-files-in-dir.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 suffix="mod"
-# location=$(pwd)/*.txt
-user_location=$1/*.txt
-if [[ ! -z $1 ]]; then
-echo "ran if"
+location=$(pwd)/*.txt
 echo "All the files Before rename"
 echo $(ls)
 echo "Selected files for rename"
-for f in ${user_location}
+for f in $location
 do
-    echo ${f}
-    extn=$(echo ${f} | cut -d "." -f 2)
-    name=$(echo ${f} | cut -d "." -f 1)
-    if [[ $(echo $name | tail -c 5 ) != "-mod" ]]; then
-    mv $f ${name}-${suffix}.${extn}
-    fi
+    echo $f
+    mv $f $f$suffix
 done
 echo "All the files After rename"
-ls -1 $1
-else
-echo "didnt run if"
-fi
+echo $(ls)
